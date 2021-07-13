e088cacdf2687f1ecb1667d5481027ba1ceda9fb
Constantinethe1st <80581493+Constantinethe1st@users.noreply.github.com>
2021-07-13 19:01:01 +0300

    130721

--- сonverter/index.js
+++ сonverter/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const multer = require('multer');
+const upload = multer();
 const mailer = require('./nodemailer');
 
 const app = express();
@@ -7,18 +9,25 @@ const app = express();
 const PORT = 3001;
 let user = undefined;
 
-app.use(express.static(__dirname + 'css/css.css'))
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static('public'))
+
+app.use(upload.none());
+
 app.post('/converter', (req, res) => {
-   if(!req.body.email || !req.body.sum || !req.body.currency || !req.body.dollarAndEuro) return res.sendStatus(400)
+	console.log(req.body);
+    if(!req.body.email || !req.body.sum || !req.body.currency || !req.body.dollarAndEuro) 
+		return res.sendStatus(400)
     const message = {
         to: req.body.email,
-        subject: 'Hello!',
-        text: 'HW!'
+        subject: 'Жду ответа, как соловей лета!)',
+        html:`
+        <h1>Запись на обмен валюты!</h1>
+        <ul>
+            <li>Рубли: ${req.body.sum}</li>
+            <li>Валюта: ${req.body.dollarAndEuro}</li>
+        </ul>`
     }
-})
-app.get('/converter', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
+	mailer(message);
 });
 
-app.listen(PORT, () => console.log(`server listening at http://localhost:${PORT}/converter`))
+app.listen(PORT, () => console.log(`server listening at http://localhost:${PORT}/converter.html`))
